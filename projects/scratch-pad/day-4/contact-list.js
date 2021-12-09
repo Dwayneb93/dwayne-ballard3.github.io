2f7871505e488afb90f8a253195aa397d91afe52
Dwayne Ballard <dwayne.ballard123@gmail.com>
2021-12-09 22:35:04 +0000

    Scratch Pad Day 4 Complete

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -22,7 +22,7 @@
  *      4. removeContact(contact): takes a contact object to be removed from 
  *         the contact-list.
  *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
- *         return a String formated with all the full-names of the separated 
+ *         return a String formated with all the full-names of then contacts separated 
  *         with a line-break, like so:
  *          
  *         myContacts.printAllContactNames(); // => Max Gaudin
@@ -35,7 +35,14 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-
+    // create a factory function here. First create and empty object literal
+    var obj = {};
+    //now create each key/value pair using dot/bracket notation
+    obj.id = id;
+    obj.nameFirst = nameFirst;
+    obj.nameLast = nameLast;
+    // return that object
+    return obj;
 } 
 
 
@@ -43,12 +50,52 @@ function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts;
+    var contacts = [];
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
+        },
+        addContact: function(contact) {
+            contacts.push(contact);
+        },
+        findContact: function(fullName) {
+            // loop over contacts array
+            for (var i = 0; i < contacts.length; i++) {
+                // check to see if fullName parameter is equal to lastName & firstName concatenated together
+                // ** DONT FORGET THE SPACE BETWEEN THE NAMES **
+                if (fullName === contacts[i].nameFirst + " " + contacts[i].nameLast) {
+                    // return the object containing the name if it exists
+                    return contacts[i];
+                } else {
+                    // return undefined if it doesn't exist
+                    return undefined;
+                }
+            }
+        },
+        removeContact: function(contact) {
+            for (var i = 0; i < contacts.length; i++) {
+                if (contact === contacts[i]) {
+                    contacts.splice(i, 1);
+                }
+            }
+        },
+        printAllContactNames: function() {
+            // create an output string to store the names
+            var str = "";
+            // loop over contacts array
+            for (var i = 0; i < contacts.length; i++) {
+                // if name isn't last name in list, add first and last names to new str followed by a line break
+                if (contacts[i] !== contacts[contacts.length - 1]) {
+                    str += contacts[i].nameFirst + " " + contacts[i].nameLast + "\n";
+                    // if name is last item in list, add first and last names to new str variable with NO line break
+                } else if (contacts[i] === contacts[contacts.length - 1]) {
+                    str += contacts[i].nameFirst + " " + contacts[i].nameLast;
+                }
+            }
+            // now, return the new string
+            return str;
         }
     }
 }
