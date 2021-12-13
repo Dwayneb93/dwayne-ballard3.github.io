9151eeec0cf53d6a9a7fb403ca144feaecb466c5
Dwayne Ballard <dwayne.ballard123@gmail.com>
2021-12-13 23:14:50 +0000

    Finished Portfolio Page

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,25 +13,60 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function search(animals, name) {
+    //loop through array
+    for (var i = 0; i < animals.length; i++) {
+        // if iterated animals name is equal to input name, then return animal object
+        if (animals[i].name === name) {
+            return animals[i];
+        }          
+    }
+    // if not, return null OUTSIDE of loop because what if first animal didnt pass?
+    // we would want the loop to still test all of the elements of the animals array
+    return null;
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function replace(animals, name, replacement) {
+    for (var i = 0; i < animals.length; i++) {
+        if (animals[i].name === name) {
+            animals[i] = replacement;
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function remove(animals, name) {
+    // loop through animals array
+    for (var i = 0; i < animals.length; i++) {
+        // if that animal with that name exists
+        if (animals[i].name === name) {
+            // remove it with splice (start index, how many to delete) start index
+            // is whatever iteration (i) and we want to delete just that one item
+            animals.splice(i, 1);
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function add(animals, animal) {
+    for (var i = 0; i < animals.length; i++) {
+        if (animals[i].name !== animal.name) {
+            animals.push(animal);
+        } else {
+            return false;
+        }
+    }
+}
 
 /**
  * You did it! You're all done with Matchy!
