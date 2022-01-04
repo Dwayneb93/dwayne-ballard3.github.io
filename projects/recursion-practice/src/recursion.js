9e0552dd46725aac8823b66cb2fe176aff271e36
Dwayne Ballard <dwayne.ballard123@gmail.com>
2022-01-04 20:37:01 +0000

    recursion

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -89,10 +89,17 @@ isEven(1)
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 
-var sumBelow = function(n, sum = 0) {
+var sumBelow = function(n) {
   // base
-
+  if (n === 0) {
+    return 0;
+  }
   // recursion
+  if (n >= 0) {
+  return n - 1 + sumBelow(n - 1);
+  } else {
+    return n + 1 + sumBelow(n + 1);
+  }
 };
 
 // 6. Get the integers in range (x, y).
